ae48bc38672299ba3edbfb47ba6f58ad019f919d
michaelperry-bimm <michaelperry@bimm.co.uk>
2025-02-05 16:53:18 +0000

    Add project files.

---
+++ NetSuiteIntegration/Program.cs
@@ -0,0 +1,6 @@
+﻿//Salesforce to NetSuite Integration, or possibly a Flywire to netsuite integration, possibly not needed. Will probably be cancelled before it's finished.
+
+
+
+
+Console.WriteLine("Hello, World!");
